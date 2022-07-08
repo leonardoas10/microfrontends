f4e5487dc41e6d59dcbc074bfd5cd96b2bc713d8
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-07-08 13:58:12 -0400

    Fixing files

--- container/src/App.js
+++ container/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
-    StylesProvider,
-    createGenerateClassName,
+  StylesProvider,
+  createGenerateClassName,
 } from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'co',
+  productionPrefix: 'co',
 });
 
 export default () => {
-    return (
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <MarketingApp />
-                </div>
-            </BrowserRouter>
-        </StylesProvider>
-    );
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
-
-//
