ca3605c5cdeb4969566dba154bd5b5c8b31b6735
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-07-11 13:18:30 -0400

    Adding: dashboard page

--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -5,29 +5,32 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: 'development',
-  output: {
-    publicPath: 'http://localhost:8082/',
-  },
-  devServer: {
-    port: 8082,
-    historyApiFallback: {
-      index: 'index.html',
+    mode: 'development',
+    output: {
+        publicPath: 'http://localhost:8083/',
     },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'auth',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './AuthApp': './src/bootstrap',
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+    devServer: {
+        port: 8083,
+        historyApiFallback: {
+            index: 'index.html',
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './DashboardApp': './src/bootstrap',
+            },
+            shared: packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
 };
 
 module.exports = merge(commonConfig, devConfig);
